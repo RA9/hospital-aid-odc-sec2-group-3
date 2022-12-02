ee9a6274869cbe6bb5a70b3caeec2f8e805e857e
Ishmael M. Dukuly <moisemadukuly82@gmail.com>
2022-12-02 15:59:19 +0000

    The footer is in progress

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+
+export default function Footer(){
+    return(
+        <React.Fragment>
+            <div className="container">
+
+            </div>
+        </React.Fragment>
+    )
+}
