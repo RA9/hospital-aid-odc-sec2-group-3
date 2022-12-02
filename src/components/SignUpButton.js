9bac32ac6cb3c48f72cf3ee22e5a0392b8759f5f
Americ Allison <americ474@gmail.com>
2022-12-02 15:00:07 +0000

    added fontawesome

--- src/components/SignUpButton.js
+++ src/components/SignUpButton.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserAlt } from '@fortawesome/free-solid-svg-icons';
 
 export default function SignUpButton() {
     return (
         <>
-          <div className='text-end mx-3 p-1'>
-            <button className='btn rounded-lg text-white bg-info'>
-               Sign Up
+          <div className='text-end mx-4 p-1'>
+            <button className='btn rounded-lg text-white bg-danger shadow-sm' onClick="#">
+            <FontAwesomeIcon icon={faUserAlt} /> Sign Up
             </button>
           </div>
         </>
