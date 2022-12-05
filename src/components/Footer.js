33821f7e4318efdb8bc38d92e3cefd4913fae5cc
Ishmael M. Dukuly <moisemadukuly82@gmail.com>
2022-12-05 05:36:24 +0000

    new update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,13 +7,13 @@ export default function Footer(){
         <React.Fragment>
             <footer id="footer">
                 <div className="footer-box">
-
+                About
                 </div>
                 <div className="footer-box">
-
+                 <h1>Contact</h1>
                 </div>
                 <div className="footer-box">
-
+                    <h1>Services</h1>
                 </div>
             </footer>
         </React.Fragment>
