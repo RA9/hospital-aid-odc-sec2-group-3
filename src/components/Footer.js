54de7a8513e2cc052308f77c24f64005621c465f
Ishmael M. Dukuly <moisemadukuly82@gmail.com>
2022-12-05 15:33:20 +0000

    adding contents to the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,22 +5,42 @@ import React from "react";
 export default function Footer(){
     return(
         <React.Fragment>
-            <footer id="footer">
+            <footer>
+            <nav id="footer-nav">
             <div className="footer-box">
                 <h3>Opening Hours</h3>
-                </div>
-            <div className="footer-box">
-                <h3>Company Name</h3>
+                <article>
+                        Monday---8:00 Am -10:00 PM
+                        Tuesday---8:00 Am -10:00 PM
+                        Wednesday---8:00 Am -10:00 PM
+                        Thursday---8:00 Am -10:00 PM
+                        Friday---8:00 Am -10:00 PM
+                    </article>
                 </div>
                 <div className="footer-box">
                 <h3>Location</h3>
+                <article>
+                    We are located in Sinkor, Oldest Conn town or visit at any of any bramches in Pynesville 
+                    and Brewerville.
+                </article>
                 </div>
                 <div className="footer-box">
-                 <h3>Menu</h3>
+                 <h3>Follow Us</h3>
                 </div>
                 <div className="footer-box">
                     <h3>Contact</h3>
+                    <article>
+                       Contact us at:<br></br>
+                       Email: hospitalaid2022@gmail.com
+                       Tel: 231-776 -69-7736
+                    </article>
                 </div>
+                <small>
+                    <p>
+                        
+                    </p>
+                </small>
+            </nav>
             </footer>
         </React.Fragment>
     )
