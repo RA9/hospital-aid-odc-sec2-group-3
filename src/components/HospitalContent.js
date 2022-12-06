884af7cd2a525cc3ddd7d368ee6e362195ee8db7
Andrew Livingstone, Jr <andrewlivingstonejr@gmail.com>
2022-12-06 22:39:24 +0000

    Verify

--- src/components/HospitalContent.js
+++ src/components/HospitalContent.js
@@ -11,30 +11,169 @@ export default function HospitalContent() {
             <div class="container">
                
                  <h1 class="text-center">ELWA HOSPITAL</h1>
-            <p class="text-center">loremk;skdf;dsafdsokrtwjlksdf;l</p>
+            <p class="text-center">loremkLorem loremkLorem loremkLorem loremkLorem</p>
 
             <div class="containerItem">
                 <div>
                 <div class="row">
                     <div class="col-md-6">
-                        <h2>Emergency</h2>
-                    <p>loremkLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                     tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-                     quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
-                      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugi
-                      at nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                       culpa qui officia deserunt mollit anim id est laborum.</p>
+                        <h2>Emergency Details</h2>
+                        <div class="accordion accordion-flush" id="accordionFlushExample">
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="flush-headingOne">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                            Accordion Item #1
+                        </button>
+                        </h2>
+                        <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                        <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion body.</div>
+                        </div>
                     </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="flush-headingTwo">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                            Accordion Item #2
+                        </button>
+                        </h2>
+                        <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+                        <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the second item's accordion body. Let's imagine this being filled with some actual content.</div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="flush-headingThree">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+                            Accordion Item #3
+                        </button>
+                        </h2>
+                        <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+                        <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
+                        </div>
+                    </div>
+                    </div>
+                                        </div>
 
-                    
+                                        
                     <div class="col-md-6">
                         <h2>Appointment</h2>
-                    <p>loremkLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                     tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-                     quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
-                      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugi
-                      at nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                       culpa qui officia deserunt mollit anim id est laborum.</p>
+
+                        <div class="formbold-main-wrapper">
+  
+  <div class="formbold-form-wrapper">
+    <form action="https://formbold.com/s/FORM_ID" method="POST">
+      <div class="formbold-mb-5">
+        <label for="name" class="formbold-form-label"> Full Name </label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          placeholder="Full Name"
+          class="formbold-form-input"
+        />
+      </div>
+      <div class="formbold-mb-5">
+        <label for="phone" class="formbold-form-label"> Phone Number </label>
+        <input
+          type="text"
+          name="phone"
+          id="phone"
+          placeholder="Enter your phone number"
+          class="formbold-form-input"
+        />
+      </div>
+      <div class="formbold-mb-5">
+        <label for="email" class="formbold-form-label"> Email Address </label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Enter your email"
+          class="formbold-form-input"
+        />
+      </div>
+      <div class="flex flex-wrap formbold--mx-3">
+        <div class="w-full sm:w-half formbold-px-3">
+          <div class="formbold-mb-5 w-full">
+            <label for="date" class="formbold-form-label"> Date </label>
+            <input
+              type="date"
+              name="date"
+              id="date"
+              class="formbold-form-input"
+            />
+          </div>
+        </div>
+        <div class="w-full sm:w-half formbold-px-3">
+          <div class="formbold-mb-5">
+            <label for="time" class="formbold-form-label"> Time </label>
+            <input
+              type="time"
+              name="time"
+              id="time"
+              class="formbold-form-input"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div class="formbold-mb-5 formbold-pt-3">
+        <label class="formbold-form-label formbold-form-label-2">
+          Address Details
+        </label>
+        <div class="flex flex-wrap formbold--mx-3">
+          <div class="w-full sm:w-half formbold-px-3">
+            <div class="formbold-mb-5">
+              <input
+                type="text"
+                name="area"
+                id="area"
+                placeholder="Enter area"
+                class="formbold-form-input"
+              />
+            </div>
+          </div>
+          <div class="w-full sm:w-half formbold-px-3">
+            <div class="formbold-mb-5">
+              <input
+                type="text"
+                name="city"
+                id="city"
+                placeholder="Enter city"
+                class="formbold-form-input"
+              />
+            </div>
+          </div>
+          <div class="w-full sm:w-half formbold-px-3">
+            <div class="formbold-mb-5">
+              <input
+                type="text"
+                name="state"
+                id="state"
+                placeholder="Enter state"
+                class="formbold-form-input"
+              />
+            </div>
+          </div>
+          <div class="w-full sm:w-half formbold-px-3">
+            <div class="formbold-mb-5">
+              <input
+                type="text"
+                name="post-code"
+                id="post-code"
+                placeholder="Post Code"
+                class="formbold-form-input"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <button class="formbold-btn">Book Appointment</button>
+      </div>
+    </form>
+  </div>
+</div>
+
                     
                 </div>
                 </div>
