8879a0929e4a33f7e12312d6f53f2c28b7ac8abb
Americ Allison <americ474@gmail.com>
2022-12-07 14:36:13 +0000

    af

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -8,17 +8,18 @@ export default function SignUpForm () {
     return (
         <form>
         <div class="mb-3">
-          <label for="Email1" class="form-label">Email address</label>
-          <input type="email" class="form-control" id="Email1" aria-describedby="emailHelp" />
+          <input type="text" class="form-control" id="hospital" aria-describedby="location1" />
           <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
         </div>
         <div class="mb-3">
-          <label for="Password1" class="form-label">Password</label>
-          <input type="password" class="form-control" id="Password1" />
+          <input type="text" class="form-control" id="Password1" />
         </div>
         <div class="mb-3 form-check">
+          <p>Emergency Response</p>
           <input type="checkbox" class="form-check-input" id="Check1" />
-          <label class="form-check-label" for="Check1">Check me out</label>
+          <label class="form-check-label" for="Check2">Check me out</label>
+          <input type="checkbox" class="form-check-input" id="Check2" />
+          <label class="form-check-label" for="Check2">Check me out</label>
         </div>
         <button type="submit" class="btn btn-success">
         <FontAwesomeIcon icon={faUserAlt} />&nbsp;Sign Up</button>
